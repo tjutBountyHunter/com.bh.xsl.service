aac1e12c022d24090551578c3eff2223c2d13f84
helinhong <tjut_hlh@qq.com>
2019-05-26 15:23:59 +0800

    上传文件

--- xsL_service_parent/xsl_service_service/src/main/java/service/impl/UserviceImpl.java
+++ xsL_service_parent/xsl_service_service/src/main/java/service/impl/UserviceImpl.java
@@ -2,70 +2,27 @@ package service.impl;
 
 import com.xsl.user.SupplementUserInfoResource;
 import com.xsl.user.UserInfoResouce;
+import com.xsl.user.vo.FileUploadReqVo;
 import com.xsl.user.vo.ResBaseVo;
 import com.xsl.user.vo.UserHMResVo;
 import com.xsl.user.vo.UserReqVo;
-import enums.UserStateEnum;
-import example.*;
-import mapper.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
-import pojo.*;
-import pojo.XslUserRegister;
 import service.*;
 import util.*;
 import vo.*;
 
 import javax.annotation.Resource;
-import java.util.*;
-
 
 @Service
 public class UserviceImpl implements UserService {
-	@Autowired
-	private XslHunterMapper xslHunterMapper;
-	@Autowired
-	private XslMasterMapper xslMasterMapper;
-    @Autowired
-    private XslUserMapper xslUserMapper;
-	@Autowired
-	private XslUserFileMapper xslUserFileMapper;
-
-	@Autowired
-	private FileOperateService fileOperateService;
-
-	@Resource
-	private ThreadPoolTaskExecutor userExecutor;
-	@Autowired
-	private TaskMqService taskMqService;
-
-	@Resource
-	private TaskInfoService taskInfoService;
-
 	@Resource
 	private SupplementUserInfoResource supplementUserInfoResource;
 	@Resource
 	private UserInfoResouce userInfoResouce;
-
-
-    @Autowired
-	private UserInfoService userInfoService;
-
-    @Value("${REDIS_USER_SESSION_KEY}")
-    private String REDIS_USER_SESSION_KEY;
-	@Value("${USER_INFO}")
-	private String USER_INFO;
-	@Value("${USER_TX_URL}")
-	private String USER_TX_URL;
-
-
 
     private static final Logger logger = LoggerFactory.getLogger(UserviceImpl.class);
 
@@ -73,14 +30,20 @@ public class UserviceImpl implements UserService {
 	public XslResult saveUserInfo(XslUserReqVo xslUserReqVo){
 		UserReqVo userReqVo = new UserReqVo();
 		BeanUtils.copyProperties(xslUserReqVo, userReqVo);
+		try {
+			ResBaseVo resBaseVo = supplementUserInfoResource.saveUserInfo(userReqVo);
 
-		ResBaseVo resBaseVo = supplementUserInfoResource.saveUserInfo(userReqVo);
+			if(resBaseVo.getStatus() != 200){
+				return XslResult.build(resBaseVo.getStatus(), resBaseVo.getMsg());
+			}
 
-		if(resBaseVo.getStatus() != 200){
-			return XslResult.build(resBaseVo.getStatus(), resBaseVo.getMsg());
+			return XslResult.ok();
+
+		}catch (Exception e){
+			e.printStackTrace();
+			return XslResult.build(500, "服务器异常");
+
 		}
-
-		return XslResult.ok();
 	}
 
 
@@ -101,7 +64,8 @@ public class UserviceImpl implements UserService {
 			return XslResult.ok(xslUserHMResVo);
 
 		}catch (Exception e){
-			throw new RuntimeException(e);
+			e.printStackTrace();
+			return XslResult.build(500, "服务器异常");
 		}
 	}
 
@@ -109,135 +73,28 @@ public class UserviceImpl implements UserService {
 	@Override
 	public XslResult upLoadUserTx(MultipartFile uploadFile, String userid){
 		//1.获取用户信息
-		XslUser userInfo = userInfoService.getUserInfo(userid);
-		if (userInfo.getUserid() == null) {
-			return XslResult.build(403, "用户不存在");
-		}
-
+		FileUploadReqVo fileUploadReqVo = new FileUploadReqVo();
 		try {
-			String userTx = userInfoService.getUserTx(userid);
-			if(!StringUtils.isEmpty(userTx)){
-				XslUserFileExample xslUserFileExample = new XslUserFileExample();
-				xslUserFileExample.createCriteria().andUseridEqualTo(userid).andTypeEqualTo("TX");
-				xslUserFileMapper.deleteByExample(xslUserFileExample);
-				JedisClientUtil.delete(USER_TX_URL + ":" + userid);
+			fileUploadReqVo.setName(uploadFile.getName());
+			fileUploadReqVo.setOriginalFilename(uploadFile.getOriginalFilename());
+			fileUploadReqVo.setBytes(uploadFile.getBytes());
+			fileUploadReqVo.setContentType(uploadFile.getContentType());
+			fileUploadReqVo.setInputStream(uploadFile.getBytes());
+			fileUploadReqVo.setSize(uploadFile.getSize());
+			fileUploadReqVo.setUserId(userid);
+			fileUploadReqVo.setSource("APP_SERVICE");
+			ResBaseVo resBaseVo = supplementUserInfoResource.upLoadUserTx(fileUploadReqVo);
+
+			if(resBaseVo.getStatus() != 200){
+				return XslResult.build(resBaseVo.getStatus(), resBaseVo.getMsg());
 			}
 
-			XslResult xslResult = fileOperateService.fileUpload(uploadFile);
-			XslFile xslFile = (XslFile)xslResult.getData();
-
-			//2.获取用户信息
-			//建立用户与文件关联
-			XslUserFile xslUserFile = new XslUserFile();
-			xslUserFile.setUserid(userInfo.getUserid());
-			xslUserFile.setFileid(xslFile.getFileid());
-			xslUserFile.setType("TX");
-
-			int insert = xslUserFileMapper.insert(xslUserFile);
-			if(insert < 1){
-				return XslResult.build(500, "服务器异常");
-			}
-
-			//3.异步更新状态
-			userExecutor.execute(() -> esUserTxurl(userid, xslFile.getUrl()));
-
-
-			return XslResult.ok(xslFile.getUrl());
+			String url = (String) resBaseVo.getExParam();
+			return XslResult.ok(url);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return XslResult.build(500, "服务器异常");
 		}
-
-	}
-
-	private void esUserTxurl(String userid, String txUrl) {
-		esUserInfo(userid, "", txUrl);
-	}
-
-	private void esUserInfo(String userid, String name, String txUrl) {
-		XslUser userInfo = userInfoService.getUserInfo(userid);
-		String masterId = userInfo.getMasterid();
-		List<XslTask> tasks = taskInfoService.getTaskByMasterId(masterId);
-
-		if(ListUtil.isNotEmpty(tasks)){
-			for(XslTask xslTask : tasks){
-				UpdateTaskVo updateTaskVo = new UpdateTaskVo();
-				if(!StringUtils.isEmpty(name)){
-					updateTaskVo.setName(name);
-				}
-				if(!StringUtils.isEmpty(txUrl)){
-					updateTaskVo.setTxUrl(txUrl);
-				}
-				updateTaskVo.setTaskId(xslTask.getTaskid());
-				taskMqService.updateEsTask(updateTaskVo);
-			}
-		}
-	}
-
-	private void initUserInfo(XslUserRegister xslUserRegister, XslUser xslUser, XslHunter xslHunter, XslMaster xslMaster) {
-		xslUser.setHunterid(xslHunter.getHunterid());
-		xslUser.setMasterid(xslMaster.getMasterid());
-		xslUser.setPhone(xslUserRegister.getPhone());
-		xslUser.setState(UserStateEnum.NA.getCode());
-		xslUser.setPassword(Md5Utils.digestMds(xslUserRegister.getPassword()));
-		xslUser.setSex("男");
-		xslUser.setName("xsl_"+xslUserRegister.getPhone());
-
-		try {
-			int result = xslUserMapper.insertSelective(xslUser);
-
-			if (result < 1){
-				throw new RuntimeException("用户信息插入失败");
-			}
-
-		}catch (Exception e){
-			e.printStackTrace();
-			throw new RuntimeException("服务器异常");
-		}
-	}
-
-	private XslMaster initXslMaster(XslUser xslUser) {
-		//初始化雇主信息
-		XslMaster xslMaster = new XslMaster();
-		xslMaster.setUserid(xslUser.getUserid());
-		xslMaster.setMasterid(UuidUtil.getUUID().substring(0, 12));
-		xslMaster.setLevel((short) 1);
-		xslMaster.setDescr("新人雇主");
-		try {
-			int result = xslMasterMapper.insertSelective(xslMaster);
-
-			if (result < 1){
-				throw new RuntimeException("雇主信息插入失败");
-			}
-
-		}catch (Exception e){
-			e.printStackTrace();
-			throw new RuntimeException("服务器异常");
-		}
-
-		return xslMaster;
-	}
-
-	private XslHunter initXslHunter(XslUser xslUser) {
-		//初始化猎人信息
-		XslHunter xslHunter = new XslHunter();
-		xslHunter.setUserid(xslUser.getUserid());
-		xslHunter.setHunterid(UuidUtil.getUUID().substring(0, 12));
-		xslHunter.setLevel((short) 1);
-		xslHunter.setDescr("新手猎人");
-		try {
-			int result = xslHunterMapper.insertSelective(xslHunter);
-
-			if (result < 1){
-				throw new RuntimeException("猎人信息插入失败");
-			}
-
-		}catch (Exception e){
-			e.printStackTrace();
-			throw new RuntimeException("服务器异常");
-		}
-
-		return xslHunter;
 	}
 
 }
--- xsL_service_parent/xsl_service_web/src/main/webapp/index.jsp
+++ xsL_service_parent/xsl_service_web/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>鸡你太美</h2>
 </body>
 </html>
